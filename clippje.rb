59c3bcc87a7d0d70adfb4ec4f27fa048cba8ac0e
Luis Parravicini <lparravi@gmail.com>
2019-03-07 09:54:59 -0300

    refactor

--- clippje.rb
+++ clippje.rb
@@ -3,61 +3,18 @@
 
 require_relative 'markov'
 require_relative 'term'
+require_relative 'screen'
 require 'io/console'
 
 
 
 RAND_OPTION = '/'
+CTRL_C = "\u0003"
 
 def read_char
     c = $stdin.getch
-
-    # ctrl-c
-    exit 1 if c == "\u0003"
-
+    exit 1 if c == CTRL_C
     c
-end
-
-
-class Screen
-  def initialize(clippje)
-    @clippje = clippje
-  end
-
-  def print_sentence
-    print Term.goto(1, 2)
-    print Term.clear_eol
-    print '> '
-    print @clippje.sentence.join(' ') + @clippje.word
-  end
-
-  def show_options
-    print Term.goto(1, 10)
-    print Term.clear_eol
-
-    unless @clippje.words.empty?
-      @clippje.words.each_with_index { |w, i| print "[#{i}. #{w}] " }
-      print "[#{RAND_OPTION} random]"
-    end
-  end
-
-  def select_option(index)
-    value = if index == RAND_OPTION
-      rand(@clippje.words.size)
-    else
-      value.to_i
-    end
-    if value < 0 || value >= @clippje.words.size
-      value = rand(@clippje.words.size)
-    end
-
-    @clippje.words[value]
-  end
-
-  def draw
-    print Term.clear_screen
-  end
-
 end
 
 
---
+++ screen.rb
@@ -0,0 +1,42 @@
+
+
+class Screen
+  def initialize(clippje)
+    @clippje = clippje
+  end
+
+  def print_sentence
+    print Term.goto(1, 2)
+    print Term.clear_eol
+    print '> '
+    print @clippje.sentence.join(' ') + @clippje.word
+  end
+
+  def show_options
+    print Term.goto(1, 10)
+    print Term.clear_eol
+
+    unless @clippje.words.empty?
+      @clippje.words.each_with_index { |w, i| print "[#{i}. #{w}] " }
+      print "[#{RAND_OPTION} random]"
+    end
+  end
+
+  def select_option(index)
+    value = if index == RAND_OPTION
+      rand(@clippje.words.size)
+    else
+      value.to_i
+    end
+    if value < 0 || value >= @clippje.words.size
+      value = rand(@clippje.words.size)
+    end
+
+    @clippje.words[value]
+  end
+
+  def draw
+    print Term.clear_screen
+  end
+
+end
